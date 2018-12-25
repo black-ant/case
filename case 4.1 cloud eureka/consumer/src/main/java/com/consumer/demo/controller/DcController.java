91239fee63e4250191c663e27d96eb7445180b45
black-ant <1016930479@qq.com>
2018-12-25 23:05:24 +0800

    eureka

---
+++ case 4.1 cloud eureka/consumer/src/main/java/com/consumer/demo/controller/DcController.java
@@ -0,0 +1,26 @@
+package com.consumer.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+public class DcController {
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+}
