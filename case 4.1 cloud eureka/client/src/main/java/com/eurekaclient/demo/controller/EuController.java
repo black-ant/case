5431fc8f32228191e8bbd6468ce5f530a9833eb9
black-ant <1016930479@qq.com>
2019-01-04 22:57:07 +0800

    断路器测试

--- case 4.1 cloud eureka/client/src/main/java/com/eurekaclient/demo/controller/EuController.java
+++ case 4.1 cloud eureka/client/src/main/java/com/eurekaclient/demo/controller/EuController.java
@@ -13,7 +13,8 @@ public class EuController {
     DiscoveryClient discoveryClient;
 
     @GetMapping("/dc")
-    public String dc() {
+    public String dc() throws InterruptedException {
+        Thread.sleep(5000L);
         String services = "Services: " + discoveryClient.getServices();
         System.out.println(services);
         return services;
