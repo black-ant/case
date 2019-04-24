f139a9df4fd47026621fbf679c4fd819349d7a61
black-ant <1016930479@qq.com>
2019-04-24 23:17:13 +0800

    add

---
+++ case 5.2 elk logsystem/src/main/java/com/elk/logsystem/LogsystemApplication.java
@@ -0,0 +1,13 @@
+package com.elk.logsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogsystemApplication.class, args);
+    }
+
+}
