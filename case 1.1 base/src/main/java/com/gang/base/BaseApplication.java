eb1a2e2aeead8361eb3224031a9642f61e7ee2a9
black-ant <1016930479@qq.com>
2019-06-18 22:51:41 +0800

    add

---
+++ case 1.1 base/src/main/java/com/gang/base/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.gang.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+
+}
