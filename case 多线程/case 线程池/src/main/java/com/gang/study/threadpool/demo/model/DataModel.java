f8f0395e068dcf2a4774f9105b2fe57634710044
曾志刚 <zengzg@paraview.cn>
2019-08-21 23:22:42 +0800

    add

--- case 多线程/case 线程池/src/main/java/com/gang/study/threadpool/demo/model/DataModel.java
+++ case 多线程/case 线程池/src/main/java/com/gang/study/threadpool/demo/model/DataModel.java
@@ -1,4 +1,6 @@
 package com.gang.study.threadpool.demo.model;
 
+import java.util.concurrent.Callable;
+
 public class DataModel {
 }
--- case 多线程/case 线程池/src/main/java/com/gang/study/threadpool/demo/service/FutureService.java
+++
@@ -1,4 +0,0 @@
-package com.gang.study.threadpool.demo.service;
-
-public class FutureService {
-}
