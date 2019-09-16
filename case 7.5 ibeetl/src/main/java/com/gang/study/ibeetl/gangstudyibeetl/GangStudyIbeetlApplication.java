a8974efe9e760f849303472dd3f98bf0b29436b6
ant-black <1016930479@qq.com>
2019-09-16 18:44:23 +0800

    add

---
+++ case 7.5 ibeetl/src/main/java/com/gang/study/ibeetl/gangstudyibeetl/GangStudyIbeetlApplication.java
@@ -0,0 +1,13 @@
+package com.gang.study.ibeetl.gangstudyibeetl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangStudyIbeetlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GangStudyIbeetlApplication.class, args);
+    }
+
+}
