dc2de9806049197eaca74509709b4490e8ec3fd0
ant-black <1016930479@qq.com>
2019-09-17 22:35:05 +0800

    add

--- case 7.5 ibeetl/src/main/java/com/gang/study/ibeetl/gangstudyibeetl/GangStudyIbeetlApplication.java
+++ case 7.5 ibeetl/src/main/java/com/gang/study/ibeetl/gangstudyibeetl/GangStudyIbeetlApplication.java
@@ -1,10 +1,15 @@
 package com.gang.study.ibeetl.gangstudyibeetl;
 
+import com.gang.study.ibeetl.gangstudyibeetl.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GangStudyIbeetlApplication {
+
+    @Autowired
+    ApplicationService applicationService;
 
     public static void main(String[] args) {
         SpringApplication.run(GangStudyIbeetlApplication.class, args);
