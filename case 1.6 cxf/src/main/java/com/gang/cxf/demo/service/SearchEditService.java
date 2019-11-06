45b79ec54ed73f9caa9b06bdd0149ebd1fe8c5e4
ant-black <1016930479@qq.com>
2019-11-06 20:27:05 +0800

    add

--- case 1.6 cxf/src/main/java/com/gang/cxf/demo/service/SearchEditService.java
+++ case 1.6 cxf/src/main/java/com/gang/cxf/demo/service/SearchEditService.java
@@ -1,13 +1,15 @@
 package com.gang.cxf.demo.service;
 
-import com.gang.cxf.demo.module.SearchCond;
-import com.gang.cxf.demo.module.SearchCondVisitor;
+import com.gang.cxf.demo.module.*;
 import org.apache.cxf.jaxrs.ext.search.*;
 import org.apache.cxf.message.MessageImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -19,42 +21,63 @@ public class SearchEditService {
 //        getSearchCond("(username==gang)");
 
 //        getSearchCond("username==gang,age==777");
-        getSearchCond("(fullName==*tgt*;fullName==*oidc*),(fullName==*oidc*)");
+        getSearchCond("(param1==*00000*;param2==*11111*),(param3==*2222*)");
+//        getSearchCond("foo==123.4,(bar==asadf*;baz=lt=20.0)");
     }
 
 
     protected void getSearchCond(final String fiql) {
         try {
             SearchContext searchContext = new SearchContextProvider().createContext(new MessageImpl());
+
+            SearchCondVisitor visitor = new SearchCondVisitor();
+
             SearchCondition<SearchBean> sc = searchContext.getCondition(fiql, SearchBean.class);
+
+            sc.accept(visitor);
+
+            SearchCond searchCond = visitor.getQuery();
             logger.info("------> this is :{} <-------", sc);
-            StringBuilder fiqlSQL = getQuery(sc.getSearchConditions());
+            logger.info("------> searchCond is :{} <-------", searchCond);
+            List<Object> parameters = Collections.synchronizedList(new ArrayList<>());
+            StringBuilder queryString = getQuery(searchCond, parameters);
+            logger.info("------> this fiql :{} <-------", queryString);
+
         } catch (Exception e) {
             throw e;
         }
     }
 
-    protected StringBuilder getQuery(final List<SearchCondition<SearchBean>> cond) {
+
+    /**
+     * 递归生成 SQL 查询 语句
+     *
+     * @param cond
+     * @return
+     */
+    protected StringBuilder getQuery1(final List<SearchCondition<SearchBean>> cond) {
         StringBuilder query = new StringBuilder();
         if (cond == null || cond.isEmpty()) {
             return query;
         }
         for (SearchCondition searchItem : cond) {
-            if (!searchItem.getSearchConditions().isEmpty()) {
-                query.append(getQuery(searchItem.getSearchConditions()));
+            if (null != searchItem.getSearchConditions() && !searchItem.getSearchConditions().isEmpty()) {
+//                query.append(getQuery(searchItem.getSearchConditions()));
                 continue;
             }
             query.append("(");
-            SearchCondition searchBean = searchItem.getCondition();
+            SearchBean searchBean = (SearchBean) searchItem.getCondition();
             switch (searchItem.getConditionType()) {
-                case AND:
-                    query.append(searchItem.getCondition());
+                case EQUALS:
+                    String key = searchBean.getKeySet().iterator().next();
+                    query.append(key).append("=").append(searchBean.get(key));
                     break;
                 case OR:
                     query.append(searchItem.getCondition());
                     break;
                 default:
             }
+            query.append(")");
 
         }
 
