f6e572f7fbc5b16f278961b3cc54f08338367d1d
ant-black <1016930479@qq.com>
2020-02-25 23:03:53 +0800

    add osgi

---
+++ case spring/case spring osgi/demo/src/main/java/com/gang/study/comgangstudyspringosgi/ComGangStudySpringOsgiApplication.java
@@ -0,0 +1,13 @@
+package com.gang.study.comgangstudyspringosgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComGangStudySpringOsgiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComGangStudySpringOsgiApplication.class, args);
+    }
+
+}
---
+++ case spring/case spring osgi/demo/src/test/java/com/gang/study/comgangstudyspringosgi/ComGangStudySpringOsgiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gang.study.comgangstudyspringosgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComGangStudySpringOsgiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
