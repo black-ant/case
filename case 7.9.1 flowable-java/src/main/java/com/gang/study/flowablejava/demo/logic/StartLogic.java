b1ee68a3fd51dbd1374c7eaf6bce4479399c3ce0
ant-black <1016930479@qq.com>
2020-03-12 23:31:42 +0800

    add

---
+++ case 7.9.1 flowable-java/src/main/java/com/gang/study/flowablejava/demo/logic/StartLogic.java
@@ -0,0 +1,10 @@
+package com.gang.study.flowablejava.demo.logic;
+
+/**
+ * @Classname StartLogic
+ * @Description TODO
+ * @Date 2020/3/12 23:15
+ * @Created by zengzg
+ */
+public class StartLogic extends Appl {
+}
