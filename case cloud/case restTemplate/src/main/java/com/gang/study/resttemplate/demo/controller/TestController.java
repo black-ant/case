1612c99fd9039b34f4e6ec8259a49efb114163d8
black-ant <1016930479@qq.com>
2020-05-19 23:28:09 +0800

    add

---
+++ case cloud/case restTemplate/src/main/java/com/gang/study/resttemplate/demo/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.gang.study.resttemplate.demo.controller;
+
+/**
+ * @Classname TestController
+ * @Description TODO
+ * @Date 2020/5/19 21:45
+ * @Created by zengzg
+ */
+public class TestController {
+}
