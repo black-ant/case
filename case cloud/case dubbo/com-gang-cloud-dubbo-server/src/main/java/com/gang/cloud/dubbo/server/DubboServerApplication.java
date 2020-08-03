a3f290f9ce395a279d57b11724cad24314dc67fb
= <1016930479@qq.com>
2020-08-03 23:27:41 +0800

    add

---
+++ case cloud/case dubbo/com-gang-cloud-dubbo-server/src/main/java/com/gang/cloud/dubbo/server/DubboServerApplication.java
@@ -0,0 +1,13 @@
+package com.gang.cloud.dubbo.server;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Classname DubboServerApplication
+ * @Description TODO
+ * @Date 2020/8/3 12:36
+ * @Created by zengzg
+ */
+@Service
+public class DubboServerApplication {
+}
