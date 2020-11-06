facd8429a09717108472d2bed8e465f636baacce
= <1016930479@qq.com>
2020-11-06 23:11:58 +0800

    add

---
+++ case 业务思路/可拖动目录设计/src/main/java/com/gang/menu/demo/MenuDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gang.menu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuDemoApplication.class, args);
+    }
+
+}
---
+++ case 业务思路/可拖动目录设计/src/test/java/com/gang/menu/demo/MenuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gang.menu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
