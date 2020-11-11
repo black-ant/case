a01d81f5225d5b84bb45c698d571ba3647423ff4
= <1016930479@qq.com>
2020-11-11 23:44:22 +0800

    add

---
+++ case 2.2.4 dm8/src/main/java/com/gang/database/demo/ComGangDatabaseDm8Application.java
@@ -0,0 +1,13 @@
+package com.gang.database.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComGangDatabaseDm8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComGangDatabaseDm8Application.class, args);
+    }
+
+}
---
+++ case 2.2.4 dm8/src/test/java/com/gang/database/demo/ComGangDatabaseDm8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gang.database.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComGangDatabaseDm8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
