118476ea8fd3f8a42e25c9acaecbacd3d8f0e5dc
= <1016930479@qq.com>
2020-12-09 23:18:30 +0800

    jol

---
+++ case 虚拟机/case jol/src/main/java/com/gang/jvm/jvmjol/JvmJolApplication.java
@@ -0,0 +1,13 @@
+package com.gang.jvm.jvmjol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmJolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvmJolApplication.class, args);
+    }
+
+}
---
+++ case 虚拟机/case jol/src/test/java/com/gang/jvm/jvmjol/JvmJolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gang.jvm.jvmjol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmJolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
