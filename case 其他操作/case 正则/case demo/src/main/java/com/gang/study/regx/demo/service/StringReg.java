228299f84a44554fa48017b19dd39bae7e411758
= <1016930479@qq.com>
2021-03-05 23:24:10 +0800

    N# New

--- case 其他操作/case 正则/case demo/src/main/java/com/gang/study/regx/demo/service/StringReg.java
+++ case 其他操作/case 正则/case demo/src/main/java/com/gang/study/regx/demo/service/StringReg.java
@@ -1,5 +1,7 @@
 package com.gang.study.regx.demo.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -15,10 +17,23 @@ import java.util.regex.Pattern;
 @Component
 public class StringReg implements ApplicationRunner {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        String regex = "<(\\w+)>(.*)</\\1>";
-        Pattern pattern = Pattern.compile(regex);
-
+        matches();
     }
+
+
+    /**
+     * 匹配指定的字符
+     */
+    public void matches() {
+
+        String pattern = ".*runoob.*";
+
+        boolean isMatch = Pattern.matches(pattern, content);
+        System.out.println("字符串中是否包含了 'runoob' 子字符串? " + isMatch);
+    }
+
 }
